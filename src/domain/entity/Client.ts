d6b51926c0fe6b41425790f0c13309300a284e9d
melvin pierre <melvin.pierre.mp@gmail.com>
2024-12-04 14:36:36 +0100

    Start entity

---
+++ src/domain/entity/Client.ts
@@ -0,0 +1,14 @@
+import { Permis } from './Permis';
+import { Profession } from './Profession';
+
+export class Client {
+    constructor(
+        public id: number,
+        public permis: Permis,
+        public profession: Profession,
+        public lastName: string,
+        public firstName: string,
+        public email: string,
+        public address: string
+    ) {}
+}
---
+++ src/domain/entity/Permis.ts
@@ -0,0 +1,18 @@
+export enum PermisStatus {
+    VALID = 'VALID',
+    EXPIRED = 'EXPIRED',
+    SUSPENDED = 'SUSPENDED'
+}
+
+export class Permis {
+    constructor(
+        public id: number,
+        public date: Date,
+        public status: PermisStatus,
+        public country: string
+    ) {
+        if (country.length !== 2) {
+            throw new Error('Country code must be exactly 2 characters.');
+        }
+    }
+}
